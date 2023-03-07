2e0ad5f10154dbf5d6f13b01b7b0d81dd1578c84
Suvir Wadhwa <115038563+suvir-wadhwa@users.noreply.github.com>
2023-03-07 02:08:48 -0500

    Add files via upload

---
+++ Lab 12/cmake-build-debug/CMakeFiles/Lab_12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_12.dir/main.cpp.o"
+  "Lab_12"
+  "Lab_12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab 14/cmake-build-debug/CMakeFiles/Lab_14.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_14.dir/main.cpp.o"
+  "Lab_14"
+  "Lab_14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
