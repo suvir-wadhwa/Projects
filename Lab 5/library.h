352a45b67def5b8f1f438e7a4195eb52561bc894
Suvir Wadhwa <115038563+suvir-wadhwa@users.noreply.github.com>
2023-03-07 02:07:16 -0500

    Add files via upload

---
+++ Lab 5/library.h
@@ -0,0 +1,6 @@
+#ifndef LAB_5_LIBRARY_H
+#define LAB_5_LIBRARY_H
+
+void hello();
+
+#endif //LAB_5_LIBRARY_H
---
+++ Lab 5/rec_05.cpp
@@ -0,0 +1,15 @@
+// Suvir Wadhwa
+// Lab 5
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int x;
+x = 10;
+cout << "x = " << x << endl;
+int* p;
+p = &x;
+cout << "p = " << p << endl;
+p = 0x0012fed4; // using the value from my Watch window
